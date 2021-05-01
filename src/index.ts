ea615c69280a7a383483e4260e3e37a5c8484a8e
Andrew Loughran <andy@zrmt.com>
2021-05-01 23:22:09 +0100

    setup custom argsS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {Command, flags} from '@oclif/command'
+import fs = require('fs')
 
 class GovernessCli extends Command {
   static description = 'describe the command here'
@@ -8,21 +9,28 @@ class GovernessCli extends Command {
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    spec: flags.string({char: 's', description: 'Filepath of the openAPI spec'}),
+    // // flag with no value (-f, --force)
+    // force: flags.boolean({char: 'f'}),
   }
 
   static args = [{name: 'file'}]
 
   async run() {
     const {args, flags} = this.parse(GovernessCli)
+    const spec = flags.spec!
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    try {
+      const data = fs.readFileSync(spec, 'utf8');
+      this.log(data);
     }
+    catch (err) {
+      this.log(err);
+    }
+
+    // if (args.file && flags.force) {
+    //   this.log(`you input --force and --file: ${args.file}`)
+    // }
   }
 }
 
